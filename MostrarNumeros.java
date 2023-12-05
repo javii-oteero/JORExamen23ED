3bdcd6446b3bea2f6e8ff997741098c7ea1e94e6
INFORMATICA\dam_orj <javii.oteero@gmail.com>
2023-12-05 12:29:37 +0100

    Añadido el main

---
+++ MostrarNumeros.java
@@ -0,0 +1,39 @@
+import java.util.Scanner; 
+
+
+
+public class MostrarNumeros { 
+
+ 
+
+    public static void main(String[] args) { 
+
+        Scanner scanner = new Scanner(System.in); 
+
+ 
+
+        System.out.println("Por favor, ingresa el primer número:"); 
+
+        double numero1 = scanner.nextDouble(); 
+
+ 
+
+        System.out.println("Ahora, ingresa el segundo número:"); 
+
+        double numero2 = scanner.nextDouble(); 
+
+ 
+
+        System.out.println("Los números ingresados son:"); 
+
+        System.out.println("Número 1: " + numero1); 
+
+        System.out.println("Número 2: " + numero2); 
+
+ 
+
+        scanner.close(); 
+
+    } 
+
+} 
